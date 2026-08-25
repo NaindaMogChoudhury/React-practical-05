01e7b66cf595e1d4635aa26667ab6b0a90e9daba
you@example.com <you@example.com>
2026-08-25 15:36:23 +0530

    Initial commit

---
+++ frontend-foundation/src/App.jsx
@@ -0,0 +1,27 @@
+// import Counter from "./components/Counter";
+// import "./App.css";
+// function App() {
+// return (
+// <div>
+// <Counter />
+// </div>
+// );
+// }
+// export default App;
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ItemList from "./components/ItemList";
+import Counter from "./components/Counter";
+import "./App.css";
+function App() {
+return (
+<div>
+<Navbar />
+<Hero />
+<ItemList />
+<Counter />
+</div>
+);
+}
+export default App;
